74298d786a0c5bf5531fe643eba770d6d6ec7edf
iaraloayza <anaiaraloayza11@gmail.com>
2025-06-05 17:26:21 -0300

    feat: :art: modularização dos estilos

---
+++ src/components/layout/MainContent.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="relative z-10">
+    <Header />
+    <HeroSection />
+    <AboutSection />
+    <ProjectsSection />
+    <SkillsSection />
+    <ContactSection />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Header from './Header.vue'
+import HeroSection from '../sections/HeroSection.vue'
+import AboutSection from '../sections/AboutSection.vue'
+import ProjectsSection from '../sections/ProjectsSection.vue'
+import SkillsSection from '../sections/SkillsSection.vue'
+import ContactSection from '../sections/ContactSection.vue'
+import Footer from './Footer.vue'
+
+export default {
+  name: 'MainContent',
+  components: {
+    Header,
+    HeroSection,
+    AboutSection,
+    ProjectsSection,
+    SkillsSection,
+    ContactSection,
+    Footer
+  }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-import './style.css' // Esta linha deve estar presente
+// Estilos globais
+import './style.css' 
+import './assets/styles/global.css'
 
 import '@fortawesome/fontawesome-free/css/all.css';
 
