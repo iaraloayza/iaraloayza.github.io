b681762fd9ece2ec47f3a31ac0d3d361daeb8d76
iaraloayza <anaiaraloayza11@gmail.com>
2025-06-30 14:34:10 -0300

    Updates

--- assets/AboutOnly-DuSHhhNO.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,A as n,c as e,r as c,o as s}from"./index-DINt6CiP.js";const r={name:"AboutOnly",components:{AboutSection:n}};function a(p,_,u,i,m,f){const o=c("AboutSection");return s(),e(o)}const A=t(r,[["render",a]]);export{A as default};
